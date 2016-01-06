4258de18d0e8e0bee8d4b8ffaf619fb941e80229
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-01-06 15:23:38 +0100

    add an http-server for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,30 @@
 /*jshint node:true*/
 module.exports = function (grunt) {
-	require('load-grunt-tasks')(grunt, [ 'grunt-*' ]);
+  require('load-grunt-tasks')(grunt, [ 'grunt-*' ]);
 
-	grunt.initConfig({
-		jshint: {
-			all: ['./*.js', './controllers/*.js'],
-			options: {
-				jshintrc: './.jshintrc'
-			}
-		}
-	});
+  grunt.initConfig({
+    jshint: {
+      all: ['./*.js', './controllers/*.js'],
+      options: {
+        jshintrc: './.jshintrc'
+      }
+    },
+    'http-server': {
+      dev: {
+        root: '.',
+        host: 'localhost',
+        port: 8080,
+        showDir: true,
+        runInBackground: false,
+        openBrowser: true
+      }
+    }
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+  });
 
-	grunt.registerTask('default', ['jshint']);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-http-server');
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('run', ['http-server:dev']);
 };
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # urireferencer_dojo
 
 Dojo widget that intereacts with a [UriRegistry](https://github.com/OnroerendErfgoed/uriregistry)
+
+You can run and test this widget locally:
+ - clone this repository
+ - run 'npm install' from the root dir
+ - run 'bower install' from the root dir
+ - run 'grunt run' from the root dir
+ - browse to the [test page](http://localhost:8080/tests/widget/test_UriReferencer.html) 
