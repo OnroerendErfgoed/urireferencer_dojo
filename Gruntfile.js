e3be09823d62b906f4c503cc9bcbef709d9f3617
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-01-06 15:41:51 +0100

    include test files in jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['./*.js', './controllers/*.js'],
+      all: ['./*.js', './controllers/*.js', './tests/**/*.js'],
       options: {
         jshintrc: './.jshintrc'
       }
