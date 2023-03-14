453c3453a58f1d91f0fa4c7fb9138f0a1a6cb6a6
yannickkuypers <kuypers.y@gmail.com>
2023-03-14 16:25:59 +0100

    Revert "#17 zichtbaarheidstekst & css toegevoegd"

---
+++ controllers/UriController.js
@@ -0,0 +1,37 @@
+/**
+ * @module controllers/UriController
+ */
+define([
+  'dojo/_base/declare',
+  'dojo/request/xhr'
+], function(
+  declare,
+  xhr
+) {
+  return declare(null, /** @lends module:controllers/UriController# */ {
+
+    uriUrl: null,
+    _target: '/references?uri=',
+
+    /**
+     * Module die instaat voor de 'ajax calls' die naar de server gemaakt worden voor het controleren van de uri.
+     * @constructs
+     * @param args Options
+     */
+    constructor: function (args) {
+      declare.safeMixin(this, args);
+    },
+
+    checkUri: function(uri) {
+      return xhr(this.uriUrl + this._target + uri, {
+        method: 'GET',
+        handleAs: 'json',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+    }
+
+  });
+});
